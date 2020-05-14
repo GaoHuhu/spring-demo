0d47c3f59e8af8ba47e6f1a3d5c330908d4ae55b
java09611806 <java09611806@kaikeba.com>
2020-05-14 23:32:49 +0800

    add aop demo

---
+++ spring_xml/src/main/java/com/gj/aspect/MyAspect.java
@@ -0,0 +1,11 @@
+package com.gj.aspect;
+
+public class MyAspect {
+    public void logAfter(){
+        System.out.println("log after");
+    }
+
+    public void logBefore(){
+        System.out.println("log before");
+    }
+}
--- spring_xml/src/test/java/com/gj/service/PersonServiceTest.java
+++ spring_xml/src/test/java/com/gj/service/PersonServiceTest.java
@@ -7,10 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import javax.annotation.Resource;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("classpath:spring.xml")
 public class PersonServiceTest {
-    @Autowired
+    @Resource
     private IPersonService personService;
 
     @Test
