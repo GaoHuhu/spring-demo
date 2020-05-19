ea739b234e9fbfe847c981734f988e9a14c19847
java09611806 <java09611806@kaikeba.com>
2020-05-19 23:32:01 +0800

    spring xml

--- spring_xml/src/test/java/com/gj/service/PersonServiceTest.java
+++ spring_xml/src/test/java/com/gj/service/PersonServiceTest.java
@@ -1,9 +1,7 @@
 package com.gj.service;
 
-import com.gj.domain.Account;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -18,5 +16,6 @@ public class PersonServiceTest {
     @Test
     public void sayHelloTest(){
         personService.sayHello();
+        System.out.println(PersonServiceTest.class.getClassLoader().getResource("spring.xml").getPath());
     }
 }
